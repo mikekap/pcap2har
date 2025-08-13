731262da9af7ba1b8485e33fe1fbeb997cd9c93f
Mike Kaplinskiy <mike@ladderlife.com>
2025-08-12 23:18:06 -0700

    websockets

---
+++ tests/test_websocket.py
@@ -0,0 +1,16 @@
+"""Tests for websocket parsing."""
+
+import pytest
+from pathlib import Path
+from pcap2har.main import *
+
+
+def parse_pcap_to_har(file):
+    return to_har_json(read_pcap_file(file))
+
+
+def test_http2_parse(golden):
+    pcap_file = Path(__file__).parent / "resources" / "websocket.pcap"
+
+    har_data = parse_pcap_to_har(str(pcap_file))
+    golden.test(har_data)
